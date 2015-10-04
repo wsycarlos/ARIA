28a56cfe6b741dfdf634f5a67b9332f457c6577b
wsycarlos <wsycarlos@126.com>
2015-10-04 15:51:17 -0230

    Submit Test Version of Web

---
+++ Client/trunk/Assets/NetTest.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+using System.Text;
+using System.Net.Sockets;
+using WebSocket4Net;
+using WebSocket4Net.Command;
+using WebSocket4Net.Protocol;
+
+public class NetTest : MonoBehaviour {
+
+    // 服务器IP地址和端口号
+    private string ServerAdress;
+
+    // 玩家名字
+    private string PlayName = "LIN";
+    //发送的消息
+    private string message;
+
+
+    //接收到 服务器发回来的消息
+
+    private string ServerMessage;
+
+
+
+    void Start()
+    {
+        // 服务器IP地址和端口号
+        ServerAdress = "ws://socketinmono-wsycarlos.rhcloud.com:8000";
+        message = "Clients Send Message";
+
+
+        ServerMessage = "Server Message";
+    }
+
+
+    void Update()
+    {/*
+        if (websocket != null && websocket.State != WebSocketState.None)
+        {
+            Debug.Log(message);
+            Debug.Log(websocket.State);
+        }
+      * */
+    }
+
+
+
+
+
+    public WebSocket websocket;
+
+    void OnGUI()
+    {
+        //绘制2个 TxtBox文本输入框
+
+        PlayName = GUI.TextField(new Rect(10, 10, 100, 20), PlayName);
+        message = GUI.TextArea(new Rect(10, 40, 200, 200), message);
+        ServerMessage = GUI.TextArea(new Rect(10, 250, 400, 200), ServerMessage);
+
+
+        //连接到服务器
+        if (GUI.Button(new Rect(250, 10, 150, 40), "Client连接"))
+        {
+            ClientSend();
+        };
+
+
+        if (websocket != null)
+        {
+            //测试向服务器发送消息
+            if (GUI.Button(new Rect(250, 60, 150, 40), "SendMessageToServer"))
+            {
+                if (message.Length < 1 || PlayName.Length < 1)
+                    return;
+
+                websocket.Send("UnityClient Send" + PlayName + "Say:" + message);
+            };
+
+
+            //断开连接按钮
+            if (GUI.Button(new Rect(250, 120, 150, 40), "CloseSocket"))
+            {
+                websocket.Close();
+                websocket = null;
+            };
+        }
+
+    }
+    public void ClientSend()
+    {
+        websocket = new WebSocket(ServerAdress);
+        websocket.Opened += new System.EventHandler(websocket_Opened);
+        //   websocket.Error += new EventHandler<ErrorEventArgs>(websocket_Error);
+        websocket.Closed += new System.EventHandler(websocket_Closed);
+        websocket.MessageReceived += new System.EventHandler<MessageReceivedEventArgs>(websocket_MessageReceived);
+
+        websocket.Open();
+    }
+
+    private void websocket_Opened(object sender, System.EventArgs e)
+    {
+        websocket.Send(PlayName + "Has Join The Game");
+    }
+
+    private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        ServerMessage += e.Message;
+    }
+
+    private void websocket_Closed(object sender, System.EventArgs e)
+    {
+        websocket.Close();
+    }
+     
+         
+     
+ 
+}
---
+++ Server/Server/Server/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperWebSocket;
+
+namespace Server
+{
+    class Program
+    {
+        static int ClientNum = 0;
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, Player> PlayerList = new Dictionary<string, Player>();
+
+            List<Player> Player__List = new List<Player>();
+
+            var appServer = new WebSocketServer();
+
+            //服务器IP地址
+                string ip = args[0];
+                int port = int.Parse(args[1]);
+
+            //Setup the appServer
+            if (!appServer.Setup(ip,port)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                return;
+            }
+
+            appServer.NewSessionConnected += new SessionHandler<WebSocketSession>(appServer_NewClientConnected);
+
+            appServer.NewMessageReceived += new SessionHandler<WebSocketSession, string>(appServer_NewMessageReceived);
+
+            appServer.SessionClosed += new SessionHandler<WebSocketSession, CloseReason>(appServer_SessionClosed);
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                return;
+            }
+
+            Console.WriteLine("服务器启动成功, 按 'q' 退出服务器!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                continue;
+            }
+
+            //Stop the appServer
+            appServer.Stop();
+
+            Console.WriteLine("The server was stopped!");
+        }
+
+
+
+        static void appServer_NewClientConnected(WebSocketSession session)
+        {
+            session.Send("第一次给客户端发信息Unity3DServer: ");
+
+            Player ps = new Player(session.SessionID);
+
+            session.Send(MakeDataToString.PlayerString(ps));
+
+            Console.WriteLine("客户端 :端口" + session.RemoteEndPoint.Port + "连接到服务器了!");
+
+            ClientNum += 1;
+
+            foreach (var ses in session.AppServer.GetAllSessions())
+            {
+                ses.Send("xxx加入了游戏");
+            }
+        }
+
+
+
+        static void appServer_NewMessageReceived(WebSocketSession session, string message)
+        {
+            session.Send("欢迎登陆本系统LinMengUnity3DServer: ");
+
+            Console.WriteLine("有客户端消息" + message);
+
+            Console.WriteLine("客户端数目" + ClientNum.ToString());
+            foreach (var ses in session.AppServer.GetAllSessions())
+            {
+                ses.Send("给所有客户端广播发送的消息LinMeng广播电台");
+            }
+        }
+
+        static void appServer_SessionClosed(WebSocketSession session, CloseReason closeRs)
+        {
+            session.Close();
+            Console.WriteLine("客户端" + session.RemoteEndPoint.Port + "断开了连接！");
+            ClientNum -= 1;
+
+            Console.WriteLine("客户端数目" + ClientNum.ToString());
+        }
+
+        public class Player
+        {
+            public string sessionID { get; set; }
+
+            public string Name { get; set; }
+
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float Z { get; set; }
+
+            public Player(string id)
+            {
+                this.sessionID = id;
+                Name = sessionID.Substring(0, 6);
+                X = -0.66666F;
+                Y = 1.59666F;
+                Z = 0;
+            }
+        }
+
+        public class MakeDataToString
+        {
+            public static string PlayerString(Player p)
+            {
+                return IDstr(p) + Namestr(p) + Xstr(p) + Ystr(p) + Zstr(p);
+            }
+
+            public static string IDstr(Player p)
+            {
+                return "<id>" + p.sessionID + "</id>";
+            }
+
+            public static string Namestr(Player p)
+            {
+                return "<name>" + p.Name + "</name>";
+            }
+
+            public static string Xstr(Player p)
+            {
+                return "<X>" + p.X + "</X>";
+            }
+
+            public static string Ystr(Player p)
+            {
+                return "<Y>" + p.Y + "</Y>";
+            }
+
+            public static string Zstr(Player p)
+            {
+                return "<Z>" + p.Z + "</Z>";
+            }
+        }
+    }
+}
